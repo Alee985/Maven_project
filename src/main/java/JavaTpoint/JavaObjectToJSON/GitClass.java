32da9626636d0d6a74eec126efe3d76c9910804e
Ali <m>
2021-07-07 18:02:10 +0500

    Main Branch

--- src/main/java/JavaTpoint/JavaObjectToJSON/GitClass.java
+++ src/main/java/JavaTpoint/JavaObjectToJSON/GitClass.java
@@ -1,4 +1,7 @@
 package JavaTpoint.JavaObjectToJSON;
 
 public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("Its calling from branch Main GitClass");
+    }
 }
